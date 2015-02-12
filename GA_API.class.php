fb70d866b0d2612666f9e0efa42d7ab1cdb1e62a
Ben Marshall <bmarshall511@gmail.com>
2015-02-11 21:05:56 -0600

    Update GA_API.class.php

--- GA_API.class.php
+++ GA_API.class.php
@@ -1,168 +1,132 @@
 <?php
+/**
+ * Include the Google API Client Library for PHP.
+ * @link https://github.com/google/google-api-php-client
+ */
+require_once( ROOT . '/lib/vendor/autoload.php' );
+
 class GA_API {
-  var $config = array(
-    'app_name'             => '',
-    'email'                => '',
-    'client_id'            => '',
-    'private_key_filename' => '',
-    'view_id'              => '',
-    'max_days'             => 7,
-    'max_calls_sec'        => 10
-  );
+	var $analytics;
+	var $config;
+	var $return = array(
+		'init_errors' => array(),
+		'hard_errors' => array(),
+		'soft_errors' => array(),
+		'queries'     => array()
+	);
 
-  var $client;
-  var $analytics;
-  var $analytics_id;
-  var $num_api_calls;
+	public function __construct( $config ) {
+		$this->config = $config;
 
-  var $return = array(
-    'soft_errors' => 0,
-    'init_errors' => 0,
-    'hard_errors' => array(),
-    'queries'     => array()
-  );
+		$client = new Google_Client();
+	    $client->setApplicationName( $this->config['app_name'] );
+	    $client->setAssertionCredentials(
+	      new Google_Auth_AssertionCredentials(
+	        $this->config['email'],
+	        array('https://www.googleapis.com/auth/analytics.readonly'),
+	        file_get_contents( $this->config['private_key_file'] )
+	      )
+	    );
 
-  public function __construct( $config, $num_api_calls = 0 ) {
-    $this->config        = array_merge( $this->config, $config );
-    $this->num_api_calls = $num_api_calls;
+	    $client->setClientId( $this->config['client_id'] );
+	    $client->setAccessType( 'offline_access' );
 
-    $this->client = new Google_Client();
-    $this->client->setApplicationName( $this->config['app_name'] );
-    $this->client->setAssertionCredentials(
-      new Google_Auth_AssertionCredentials(
-        $this->config['email'],
-        array('https://www.googleapis.com/auth/analytics.readonly'),
-        file_get_contents( $this->config['private_key_filename'] )
-      )
-    );
+	    $this->analytics = new Google_Service_Analytics( $client );
+	}
 
-    $this->client->setClientId( $this->config['client_id'] );
-    $this->client->setAccessType( 'offline_access' );
+	public function call( $args ) {
+		$param = array();
 
-    $this->analytics    = new Google_Service_Analytics( $this->client );
-    $this->analytics_id = 'ga:' . $this->config['view_id'];
-  }
+		$start_date = isset( $args['start_date'] ) ? $args['start_date'] : false;
+		$end_date   = isset( $args['end_date'] ) ? $args['end_date'] : false;
+		$metrics    = isset( $args['metrics'] ) ? $args['metrics'] : false;
 
-  public function call( $args ) {
-    $param       = array();
-    $start_date  = isset( $args['start_date'] ) ? $args['start_date'] : false;
-    $end_date    = isset( $args['end_date'] ) ? $args['end_date'] : false;
-    $metrics     = isset( $args['metrics'] ) ? $args['metrics'] : false;
+		if( isset( $args['dimensions'] ) ) $param['dimensions'] = isset( $args['dimensions'] ) ? $args['dimensions'] : false;
+		if( isset( $args['sort'] ) ) $param['sort'] = isset( $args['sort'] ) ? $args['sort'] : false;
+		if( isset( $args['filters'] ) ) $param['filters'] = isset( $args['filters'] ) ? $args['filters'] : false;
 
-    if( ! $start_date ) {
-      $this->return['hard_errors'][] = 'Missing start date.';
-    }
+		$param['max-results'] = isset( $args['max_results'] ) ? $args['max_results'] : 1000;
 
-    if( ! $end_date ) {
-      $this->return['hard_errors'][] = 'Missing end date.';
-    }
+		if( ! $start_date ) $this->return['init_errors'] = 'Missing start date.';
+		if( ! $end_date ) $this->return['init_errors'] = 'Missing end date.';
+		if( ! $metrics ) $this->return['init_errors'] = 'At least one metric must be defined.';
 
-    if( ! $metrics ) {
-      $this->return['hard_errors'][] = 'You must define at least one metric.';
-    }
+		if( ! count( $this->return['init_errors'] ) ) {
+			// If using custom dimensions and date range exceeds the max_days limit,
+      		// build a date array to perform multiple API calls to avoid data sampling.
+			$diff = intval( strtotime( $start_date ) - strtotime( $end_date ) ) * -1;
+      		$diff = floor( $diff / ( 60 * 60 * 24 ) );
 
-    $param['max-results'] = isset( $args['max_results'] ) ? $args['max_results'] : 1000;
+      		// Data sampling is only applied to custom dimensions.
+      		if( isset( $param['dimensions'] ) && $diff > $this->config['max_days'] ) {
+      			$date_range = $this->_date_range( $start_date, $end_date );
 
-    if( isset( $args['dimensions'] ) ) {
-      $param['dimensions']  = isset( $args['dimensions'] ) ? $args['dimensions'] : false;
-    }
+      			$cnt = 1;
+		        foreach( $date_range as $key => $date ) {
+		          if( 1 == $cnt ) {
+		            $start_date = $date;
+		          } elseif( $cnt == $this->config['max_days'] ) {
+		            $end_date = $date;
+		            $cnt      = 0;
+		            $time_key = strtotime( $start_date ) . '-' . strtotime( $end_date );
 
-    if( isset( $args['sort'] ) ) {
-      $param['sort'] = isset( $args['sort'] ) ? $args['sort'] : false;
-    }
+		            $this->return['queries'][$time_key] = $this->_call( $start_date, $end_date, $metrics, $param );
+		          }
+		          $cnt++;
+		        }
+      		} else {
+      			$time_key = strtotime( $start_date ) . '-' . strtotime( $end_date );
+       			$this->return['queries'][$time_key] = $this->_call( $start_date, $end_date, $metrics, $param );
+      		}
+		}
 
-    if( isset( $args['filters'] ) ) {
-      $param['filters'] = isset( $args['filters'] ) ? $args['filters'] : false;
-    }
+		return $this->return;
+	}
 
-    if( ! count( $this->return['hard_errors'] ) ) {
-      // If using custom dimensions and date range exceeds the max_days limit,
-      // build a date array to perform multiple API calls to avoid data sampling.
-      $diff = intval( strtotime( $start_date ) - strtotime( $end_date ) ) * -1;
-      $diff = floor( $diff / ( 60 * 60 * 24 ) );
+	private function _call( $start_date, $end_date, $metrics, $param ) {
+		$return = array(
+			'soft_errors' => array(),
+			'init_errors' => array(),
+			'total_results' => 0,
+			'total_metric_results' => array(),
+			'data' => array()
+		);
 
-      // Data sampling is only applied to custom dimensions.
-      if( isset( $param['dimensions'] ) && $diff > $this->config['max_days'] ) {
-        $date_range = $this->_date_range( $start_date, $end_date );
-
-        $cnt = 1;
-        foreach( $date_range as $key => $date ) {
-          if( 1 == $cnt ) {
-            $start_date = $date;
-          } elseif( $cnt == $this->config['max_days'] ) {
-            $end_date = $date;
-            $cnt      = 0;
-            $time_key = strtotime( $start_date ) . '-' . strtotime( $end_date );
-
-            $this->return['queries'][$time_key] = $this->_call( $start_date, $end_date, $metrics, $param );
-          }
-          $cnt++;
-        }
-      } else {
-        $time_key = strtotime( $start_date ) . '-' . strtotime( $end_date );
-        $this->return['queries'][$time_key] = $this->_call( $start_date, $end_date, $metrics, $param );
-      }
-    }
-
-    return $this->return;
-  }
-
-  private function _call( $start_date, $end_date, $metrics, $param ) {
-    $return = array(
-      'init_errors' => array(),
-      'hard_errors' => array(),
-      'soft_errors' => array(),
-      'data'        => array()
-    );
-
-    if( $this->num_api_calls == $this->config['max_calls_sec'] ) {
-      // Max. API calls per second reached, wait 1 second before the next set of
-      // calls.
-      sleep(1);
-    }
-
-    $this->num_api_calls++;
-
-    try {
-      $result = $this->analytics->data_ga->get( $this->analytics_id,
+		try {
+			$result = $this->analytics->data_ga->get( 'ga:' . $this->config['view_id'],
                 date( 'Y-m-d', strtotime( $start_date ) ),
                 date( 'Y-m-d', strtotime( $end_date ) ),
                 $metrics, $param );
 
-      if( !$result->containsSampledData ) {
-        $this->return['soft_errors']++;
-        $return['soft_errors'][] = 'Sampled data returned. Please decrease the max_days limit configuration setting until you no longer see this message.';
-      } else {
-        if( $result->getRows() ) {
-          $this->return['queries'];
-          $return['data'] = $result->getRows();
-        } else {
-          $this->return['soft_errors']++;
-          $return['soft_errors'][] = 'No data available.';
-        }
-      }
-    } catch( Exception $e ) {
-      $this->return['init_errors']++;
-      $return['init_errors'][] = 'There was an error : - ' . $e->getMessage();
-    }
+			// Check for sampled data.
+			if( $result->containsSampledData ) {
+				$return['soft_errors'][] = 'Sampled data returned. Please decrease the max_days limit configuration setting until you no longer see this message.';
+			}
 
-    return $return;
-  }
+			$return['total_results'] = $result->totalResults;
+			$return['total_metric_results'] = $result->totalsForAllResults;
+			if( $result->rows ) $return['data'] = $result->rows;
+		} catch( Exception $e ) {
+			$return['init_errors'][] = 'There was an error : - ' . $e->getMessage();
+		}
 
-  private function _date_range( $start, $end ) {
-    $range = array();
+		return $return;
+	}
 
-    if( is_string( $start ) === true ) $start = strtotime( $start );
-    if( is_string( $end ) === true ) $end = strtotime( $end );
+	private function _date_range( $start_date, $end_date ) {
+		$range = array();
 
-    if( $start > $end ) return $this->_date_range( $end, $start );
+	    if( is_string( $start_date ) === true ) $start_date = strtotime( $start_date );
+	    if( is_string( $end_date ) === true ) $end_date = strtotime( $end_date );
 
-    do {
-      $range[] = date( 'Y-m-d', $start );
-      $start = strtotime( '+ 1 day', $start );
-    }
-    while( $start < $end );
+	    if( $start_date > $end_date ) return $this->_date_range( $end_date, $start_date );
 
-    return $range;
-  }
+	    do {
+	      $range[] = date( 'Y-m-d', $start_date );
+	      $start_date = strtotime( '+ 1 day', $start_date );
+	    }
+	    while( $start_date < $end_date );
+
+	    return $range;
+	}
 }
